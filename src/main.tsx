038a949c64cf6d4b5af0b657300c12de6fbdadcb
shatabdi12 <shatabdi.dey12@gmail.com>
2025-07-22 17:20:00 +0200

    feat: add sentry setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import * as Sentry from '@sentry/react';
+
+Sentry.init({
+  dsn: 'YOUR_DSN_HERE',
+  integrations: [
+    Sentry.browserTracingIntegration(), // ✅ Use this new method
+  ],
+  tracesSampleRate: 1.0,
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
